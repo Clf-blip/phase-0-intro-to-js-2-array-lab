ef146ffac6e30399dbe29c2789968f2ad9c7ab4d
jonathan <cliffjonathan66@gmail.com>
2025-03-03 18:31:15 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+// Declare the array only once
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions (modify `cats` directly)
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Non-destructive functions (return a new array)
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+// Export functions (if required for testing)
+module.exports = {
+    cats,
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveLastCat,
+    destructivelyRemoveFirstCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat
+};
